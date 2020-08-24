65cb84c800557c84a2fd41e6c98d48321de935fe
Renato Santos Morais <renato.s.morais@hotmail.com>
2020-08-24 09:53:30 -0300

    teste

---
+++ api/cliente.js
@@ -0,0 +1,34 @@
+const listarClientes = () => {
+    return fetch("http://localhost:4000/clientes", {
+      method: "get"
+    })
+      .then(resposta => {
+        return resposta.json();
+      })
+      .then(json => {
+        return json;
+      });
+  };
+
+const cadastrarClientes = (nome,cpf) => { 
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf
+
+  })
+  return fetch('http://localhost:4000/clientes/cliente',{
+    method: 'POST',
+    headers:{
+      'Content-type': 'application/json'
+    },
+
+    body: json
+  })
+  .then( resp => {
+    return resposta.body
+
+
+  })
+    
+ 
+}
---
+++ cadastro-clientes.js
@@ -0,0 +1,12 @@
+const formCadastroCliente = document.querySelector('[data-form]')
+
+formCadastroCliente.addEventListener("submit", event => { 
+    event.preventDefault()
+
+    const nome = event.target.querySelector('[data-nome]')
+    const cpf = event.target.querySelector('[data-cpf]')
+
+    cadastrarClientes(nome.value, cpf.value)
+
+
+})
